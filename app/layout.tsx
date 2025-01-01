d7494dc703e46eabe9dc3f492d2f2d6b8d3173ab
pavanydg <pavanyermunja@gmail.com>
2025-01-01 21:54:32 +0530

    added schema

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import { ConvexClientProvider } from "@/components/providers/convex-provider";
+import { Toaster } from 'sonner'
 
 const geistSans = Geist({
     variable: "--font-geist-sans",
@@ -45,6 +46,7 @@ export default function RootLayout({
                         disableTransitionOnChange
                         storageKey="flowtion-theme-2"
                     >
+                        <Toaster position="bottom-center"/>
                         {children}
                     </ThemeProvider>
                 </ConvexClientProvider>
